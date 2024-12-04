e99451737da22215e086bdb8ebb08a7ef45bfba2
Min-swo <msms0226@g.skku.edu>
2024-12-04 12:42:05 +0900

    [Chore]  🍎nginx 설정 수정

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
 # 1. Nginx를 백그라운드로 실행
-nginx &
+# nginx &
 
-# 2. Certbot으로 인증서 발급 (Webroot 사용)
-certbot certonly --webroot -w /usr/share/nginx/html \
-  -d schedulehanaro.digital -d www.schedulehanaro.digital \
-  --non-interactive --agree-tos -m likesun2000@gmail.com
+# # 2. Certbot으로 인증서 발급 (Webroot 사용)
+# certbot certonly --webroot -w /usr/share/nginx/html \
+#   -d schedulehanaro.digital -d www.schedulehanaro.digital \
+#   --non-interactive --agree-tos -m likesun2000@gmail.com
 
 # 3. 인증서가 발급되면 Nginx를 다시 로드
-nginx -s reload
+# nginx -s reload
 
 # 4. 포그라운드에서 Nginx 실행
 nginx -g "daemon off;"
