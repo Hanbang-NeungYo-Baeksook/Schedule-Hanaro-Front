11109d45dc4f3546fa9ff253605e22677f540adf
Min-swo <msms0226@g.skku.edu>
2024-12-06 15:00:49 +0900

    [Fix] 🍎 길찾기 페이지 반응형 디자인 수정

--- src/components/Direction/TopSheet/DepartureArrivalAddress.tsx
+++ src/components/Direction/TopSheet/DepartureArrivalAddress.tsx
@@ -12,7 +12,7 @@ export default function DepartureArrivalAddress({
   const branchIdx = BRANCH_MOCK.findIndex(({ id }) => id === branchId);
 
   return (
-    <div className='flex flex-col justify-between gap-1 text-left'>
+    <div className='flex w-fit flex-col justify-between gap-1 text-left'>
       <div className='flex flex-col'>
         <div className='text-xs text-gray-400'>출발지</div>
         <div className='font-bold'>{startAddress || ''}</div>
