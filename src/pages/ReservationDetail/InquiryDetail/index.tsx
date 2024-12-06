369df8a94a61aa080e9fa2b2c91abb65026126fd
yena <yeena114@naver.com>
2024-12-06 17:38:27 +0900

    [Fix] 🐾 답변 상세 페이지 위치 수정

--- src/pages/ReservationDetail/InquiryDetail/index.tsx
+++ src/pages/ReservationDetail/InquiryDetail/index.tsx
@@ -57,34 +57,30 @@ export function InquiryDetailPage() {
 
   return (
     <>
-      <>
-        {renderHeader}
-        <div className='mx-auto w-[90%] pt-[6rem]'>
-          <div className='flex w-full flex-col items-center gap-[3rem]'>
-            {renderInquiryContent}
-            {renderDivider}
-            {inquiry2 ? (
-              <div className='flex flex-col gap-[0.5rem] text-left'>
-                <label className='text-2xl font-bold'>답변 내용</label>
-                <div className='text-sm text-[#B3B3B3]'>
-                  {inquiry2.date2} • {inquiry2.time2}
-                </div>
-                <div className='text-lg text-[#464646]'>
-                  {inquiry2.content2}
-                </div>
+      {renderHeader}
+      <div className='spacrem] mx-auto w-[90%] pb-[7rem] pt-[5rem]'>
+        <div className='flex w-full flex-col items-center gap-[3rem]'>
+          {renderInquiryContent}
+          {renderDivider}
+          {inquiry2 ? (
+            <div className='flex flex-col gap-[0.5rem] text-left'>
+              <label className='text-2xl font-bold'>답변 내용</label>
+              <div className='text-sm text-[#B3B3B3]'>
+                {inquiry2.date2} • {inquiry2.time2}
               </div>
-            ) : (
-              <div className='flex w-full flex-col items-center gap-[1rem]'>
-                <img src={waitingAnswer} alt='Waiting for answer' />
-                <div className='text-center text-xl font-bold'>
-                  상담사가 고객님의 문의를 처리 중입니다.
-                </div>
+              <div className='text-lg text-[#464646]'>{inquiry2.content2}</div>
+            </div>
+          ) : (
+            <div className='flex w-full flex-col items-center gap-[1rem]'>
+              <img src={waitingAnswer} alt='Waiting for answer' />
+              <div className='text-center text-xl font-bold'>
+                상담사가 고객님의 문의를 처리 중입니다.
               </div>
-            )}
-          </div>
+            </div>
+          )}
         </div>
-        <Nav />
-      </>
+      </div>
+      <Nav />
     </>
   );
 }
