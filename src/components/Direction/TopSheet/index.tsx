b541a061301d8d6c39c02918d480c77bb75851d2
Min-swo <msms0226@g.skku.edu>
2024-12-06 15:59:15 +0900

    [Design] 🍎 길찾기 페이지 반응형 디자인

--- src/components/Direction/TopSheet/index.tsx
+++ src/components/Direction/TopSheet/index.tsx
@@ -14,7 +14,7 @@ type TopSheetProps = {
 
 export default function TopSheet({ closeDirection, branchId }: TopSheetProps) {
   return (
-    <div className='absolute top-4 z-10 h-[13rem] w-[70%] max-w-[30rem] rounded-xl bg-white px-6 py-4 max-[800px]:w-[52.5%] max-[715px]:pr-[0rem] max-[685px]:w-[57%] max-[630px]:w-[60%] max-[600px]:w-[72%] max-[565px]:w-[75%] max-[511px]:w-[80%] max-[480px]:w-[85%] max-[450px]:w-[90%] max-[400px]:w-[95%] max-[380px]:w-[97%] max-[370px]:w-[98%]'>
+    <div className='absolute top-4 z-10 h-[13rem] w-[70%] max-w-[30rem] rounded-xl bg-white px-6 py-4 max-[800px]:w-[52.5%] max-[685px]:w-[57%] max-[630px]:w-[60%] max-[600px]:w-[72%] max-[565px]:w-[75%] max-[511px]:w-[80%] max-[480px]:w-[85%] max-[450px]:w-[90%] max-[400px]:w-[95%] max-[380px]:w-[97%] max-[370px]:w-[98%] max-[325px]:px-3'>
       <Button
         variant={'link'}
         className='fixed right-4 top-5 w-auto'
@@ -23,12 +23,12 @@ export default function TopSheet({ closeDirection, branchId }: TopSheetProps) {
         <X />
       </Button>
       <div className='flex h-full flex-col justify-between'>
-        <div className='flex h-2/3 justify-start gap-5'>
+        <div className='flex h-2/3 justify-start gap-5 max-[400px]:gap-3'>
           <DepartureArrivalTime />
           <DirectionBar />
           <DepartureArrivalAddress branchId={branchId} />
         </div>
-        <div className='flex w-full items-center justify-evenly'>
+        <div className='max-[400px]: flex w-full items-center justify-evenly'>
           <TotalTime />
           <Separator orientation='vertical' />
           <TotalDistance />
