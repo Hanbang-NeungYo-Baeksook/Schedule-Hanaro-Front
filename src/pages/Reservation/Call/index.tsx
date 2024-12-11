e7fc5381e7caef68012cb47721509ddc5a784a92
Min-swo <msms0226@g.skku.edu>
2024-12-11 14:35:16 +0900

    [Design] 🍎 방문/전화/1:1 예약 목록 페이지 반응형 디자인 적용

--- src/pages/Reservation/Call/index.tsx
+++ src/pages/Reservation/Call/index.tsx
@@ -1,13 +1,8 @@
 import '@/index.css';
 import CallList from '@/components/Reservation/CallList';
 import { callListData } from '@/mock/mockReservationCall';
-import { ReactComponent as RefreshIcon } from '@/assets/icons/reservation/refresh.svg';
 
 export function ReservationCallPage() {
-  const refreshButtonClick = () => {
-    // 추후 새로고침 기능 추가
-  };
-
   return (
     <>
       <div className='mx-auto flex h-screen w-[100%] flex-col pb-[120px]'>
@@ -17,14 +12,7 @@ export function ReservationCallPage() {
           ))}
         </div>
 
-        <div className='relative pb-[142px] min-[435px]:pb-[160px] min-[800px]:pb-[180px]'>
-          <button
-            onClick={refreshButtonClick}
-            className='absolute bottom-[170px] right-4 z-40 flex h-[5rem] w-[5rem] min-[435px]:bottom-[180px] min-[800px]:bottom-[200px]'
-          >
-            <RefreshIcon />
-          </button>
-        </div>
+        <div className='relative pb-[142px] min-[435px]:pb-[160px] min-[800px]:pb-[180px]'></div>
       </div>
     </>
   );
--- src/pages/Reservation/Inquiry/index.tsx
+++ src/pages/Reservation/Inquiry/index.tsx
@@ -5,14 +5,13 @@ import InquiryList from '@/components/Reservation/InquiryList';
 export function ReservationInquiryPage() {
   return (
     <>
-      <div className='mx-auto flex w-[90%] items-center justify-center'>
-        <div className='flex w-full flex-col'>
-          <div className='space-y-[1.5rem]'>
-            {inquiryListData.map((call, index) => (
-              <InquiryList key={index} {...call} idx={call.id} />
-            ))}
-          </div>
+      <div className='mx-auto flex h-screen w-[100%] flex-col pb-[120px]'>
+        <div className='flex h-full w-full flex-col space-y-[1.5rem] overflow-auto py-[1rem] scrollbar-hide'>
+          {inquiryListData.map((call, index) => (
+            <InquiryList key={index} {...call} idx={call.id} />
+          ))}
         </div>
+        <div className='relative pb-[142px] min-[435px]:pb-[160px] min-[800px]:pb-[180px]'></div>
       </div>
     </>
   );
