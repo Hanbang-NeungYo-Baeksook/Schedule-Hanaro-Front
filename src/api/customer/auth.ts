de2ced3ecf0456d9f0094576bfc8c4a9b78e7dcd
Min-swo <msms0226@g.skku.edu>
2024-12-23 16:05:08 +0900

    [Feat] 🍎 Auth API 구현

--- src/api/customer/auth.ts
+++ src/api/customer/auth.ts
@@ -3,18 +3,38 @@ import apiCall from '../Api';
 
 const BASE_URL = API_ROUTE.customer + '/auth';
 
-export type PostLoginRequest = {
+export type PostSignInRequest = {
   authId: string;
   password: string;
 };
 
-export type PostLoginResponse = {
+export type PostSignInResponse = {
   accessToken: string;
 };
 
-export const postLogin = async ({ authId, password }: PostLoginRequest) => {
+export type PostSignUpRequest = {
+  authId: string;
+  password: string;
+  name: string;
+  phoneNum: string;
+  birth: string;
+  gender: string;
+};
+
+export type PostSignUpResponse = {
+  message: string;
+};
+
+export const postSignIn = async ({ authId, password }: PostSignInRequest) => {
   return (await apiCall.post(BASE_URL + '/sign-in', {
     authId,
     password,
-  })) as PostLoginResponse;
+  })) as PostSignInResponse;
+};
+
+export const postSignUp = async (postSignUpRequest: PostSignUpRequest) => {
+  return (await apiCall.post(
+    BASE_URL + '/sign-up',
+    postSignUpRequest
+  )) as PostSignUpResponse;
 };
--- src/hooks/query/customer/usePostSignIn.ts
+++ src/hooks/query/customer/usePostSignIn.ts
@@ -1,4 +1,4 @@
-import { postLogin } from '@/api/customer/auth';
+import { postSignIn } from '@/api/customer/auth';
 import { QUERY_KEYS } from '@/constants/queryKeys';
 import { showToast } from '@/pages';
 import { useMutation } from '@tanstack/react-query';
@@ -6,13 +6,13 @@ import { useNavigate } from 'react-router-dom';
 import { useToast } from '../../use-toast';
 import { TokenNames } from '@/api/Api';
 
-const usePostLoginQuery = () => {
+const usePostSignIn = () => {
   const { toast } = useToast();
   const navigate = useNavigate();
 
   return useMutation({
-    mutationKey: [QUERY_KEYS.LOGIN],
-    mutationFn: postLogin,
+    mutationKey: [QUERY_KEYS.SIGN_IN],
+    mutationFn: postSignIn,
     onSuccess: ({ accessToken }) => {
       console.log(accessToken);
       const tokenName: TokenNames = 'customerAccessToken';
@@ -23,4 +23,4 @@ const usePostLoginQuery = () => {
   });
 };
 
-export default usePostLoginQuery;
+export default usePostSignIn;
