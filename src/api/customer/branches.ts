b8db10c2dbf08152bab68d04331c164181b8cf0d
Min-swo <msms0226@g.skku.edu>
2024-12-24 17:18:42 +0900

    [Feat] 🍎 추천 영업점 목록 API 구현

--- src/api/customer/branches.ts
+++ src/api/customer/branches.ts
@@ -37,6 +37,30 @@ export type GetBranchDetailRequest = {
 
 export type GetBranchDetailResponce = BranchData;
 
+export type TransportType = 'WALK' | 'CAR';
+
+export type SectionType = 'DEPOSIT' | 'PERSONAL_LOAN' | 'OTHERS';
+
+export type GetBranchRecommendListRequest = {
+  latitude: number;
+  longitude: number;
+  transportType: TransportType;
+  sectionType: SectionType;
+};
+
+export type BranchRecommendData = {
+  branch_id: number;
+  branch_name: string;
+  address: string;
+  distance: number;
+  wait_time: number;
+  current_num: number;
+};
+
+export type GetBranchRecommendListResponse = {
+  recommend_list: BranchRecommendData[];
+};
+
 export const getBranchList = async ({
   latitude,
   longitude,
@@ -51,4 +75,13 @@ export const getBranchDetail = async ({
   return (await apiCall.get(
     BASE_URL + '/' + branch_id
   )) as GetBranchDetailResponce;
+};
+
+export const getBranchRecommendList = async (
+  getBranchRecommendListRequest: GetBranchRecommendListRequest
+) => {
+  return (await apiCall.get(
+    BASE_URL + '/recommend',
+    getBranchRecommendListRequest
+  )) as GetBranchRecommendListResponse;
 };
--- src/components/Map/RecBranch.tsx
+++ src/components/Map/RecBranch.tsx
@@ -1,11 +1,10 @@
-import { REC_BRANCH_MOCK } from '@/mock/branch_mock';
 import RecBranchBox from './RecBranchBox';
 import { Badge } from '../ui/badge';
 import { ReactComponent as PedestrainIcon } from '@/assets/icons/pedestrain.svg';
 import { ReactComponent as PedestrainWhiteIcon } from '@/assets/icons/pedestrainWhite.svg';
 import { ReactComponent as AutomobileIcon } from '@/assets/icons/automobile.svg';
 import { ReactComponent as AutomobileWhiteIcon } from '@/assets/icons/automobileWhite.svg';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import {
   Select,
   SelectContent,
@@ -13,24 +12,63 @@ import {
   SelectTrigger,
   SelectValue,
 } from '../ui/select';
+import useGetBranchRecommendList from '@/hooks/query/customer/useGetBranchRecommendList';
+import { SectionType, TransportType } from '@/api/customer/branches';
+import { useMap } from '@/hooks/map-context';
 
 function RecBranch() {
-  const [selectedPath, setSelectedPath] = useState<'pedestrain' | 'automobile'>(
-    'pedestrain'
-  );
+  const [transportType, setTransportType] = useState<TransportType>('WALK');
+  const sectionType = useRef<SectionType>('DEPOSIT');
+  const { getCurrentLatitude, getCurrentLongitude } = useMap();
+
+  const setSectionType = (type: SectionType) => {
+    sectionType.current = type;
+  };
+
+  const { data: branchRecommendList, isLoading } = useGetBranchRecommendList({
+    latitude: getCurrentLatitude(),
+    longitude: getCurrentLongitude(),
+    transportType: transportType,
+    sectionType: sectionType.current,
+  });
+
+  if (isLoading) {
+    return <>Loading...</>;
+  }
+
+  if (!branchRecommendList) {
+    return <>No Recommend</>;
+  }
+
+  const convertValueToItem = (type: SectionType) => {
+    if (type === 'DEPOSIT') {
+      return '예금';
+    } else if (type === 'PERSONAL_LOAN') {
+      return '개인대출';
+    } else {
+      return '기업대출';
+    }
+  };
+
   return (
     <div className='flex w-full flex-col items-start justify-center py-3 text-left'>
       <div className='flex w-full justify-between'>
         <span className='font-regular text-[1.125rem]'>실시간 추천 영업점</span>
         <div className='flex h-[90%] cursor-pointer items-center gap-1'>
-          <Select>
+          <Select
+            onValueChange={(value: string) =>
+              setSectionType(value as SectionType)
+            }
+          >
             <SelectTrigger className='z-[61] space-x-1 border-none text-lightGrey'>
-              <SelectValue placeholder='예금' />
+              <SelectValue
+                placeholder={convertValueToItem(sectionType.current)}
+              />
             </SelectTrigger>
             <SelectContent className='right-8 z-[61]'>
-              <SelectItem value='예금'>예금</SelectItem>
-              <SelectItem value='개인대출'>개인대출</SelectItem>
-              <SelectItem value='기업대출'>기업대출</SelectItem>
+              <SelectItem value='DEPOSIT'>예금</SelectItem>
+              <SelectItem value='PERSONAL_LOAN'>개인대출</SelectItem>
+              <SelectItem value='OTHERS'>기업대출</SelectItem>
             </SelectContent>
           </Select>
         </div>
@@ -42,20 +80,20 @@ function RecBranch() {
         variant='white'
         className='mt-1 h-[2.25rem] w-full px-0 py-0 text-[0.875rem] tracking-wider'
         onClick={() => {
-          if (selectedPath === 'pedestrain') {
-            setSelectedPath('automobile');
+          if (transportType === 'WALK') {
+            setTransportType('CAR');
           } else {
-            setSelectedPath('pedestrain');
+            setTransportType('WALK');
           }
         }}
       >
         <div className='flex h-full w-full gap-2'>
           <Badge
-            variant={selectedPath === 'pedestrain' ? 'dark' : 'white'}
+            variant={transportType === 'WALK' ? 'dark' : 'white'}
             className='h-full w-full border-none py-1 text-[1.225rem] tracking-wider'
           >
             <div className='mx-auto flex items-center justify-center gap-1'>
-              {selectedPath === 'pedestrain' ? (
+              {transportType === 'WALK' ? (
                 <PedestrainWhiteIcon />
               ) : (
                 <PedestrainIcon />
@@ -64,11 +102,11 @@ function RecBranch() {
             </div>
           </Badge>
           <Badge
-            variant={selectedPath === 'pedestrain' ? 'white' : 'dark'}
+            variant={transportType === 'WALK' ? 'white' : 'dark'}
             className='h-full w-full border-none py-1 text-[1.225rem] tracking-wider'
           >
             <div className='mx-auto flex w-full items-center justify-center gap-1'>
-              {selectedPath === 'pedestrain' ? (
+              {transportType === 'WALK' ? (
                 <AutomobileIcon />
               ) : (
                 <AutomobileWhiteIcon />
@@ -79,17 +117,32 @@ function RecBranch() {
         </div>
       </Badge>
       <ul className='flex w-full items-stretch gap-4 overflow-x-scroll p-2'>
-        {REC_BRANCH_MOCK.map(({ branchId, branchName, location, distance }) => (
-          <li key={branchId} className='w-[40%] flex-shrink-0 items-stretch'>
-            <RecBranchBox
-              branchId={branchId}
-              branchName={branchName}
-              location={location}
-              distance={distance}
-              waitingTime={23}
-            />
-          </li>
-        ))}
+        {branchRecommendList.recommend_list.length === 0 ? (
+          <span className='mx-auto'>주변에 추천할 영업점이 없습니다.</span>
+        ) : (
+          branchRecommendList.recommend_list?.map(
+            ({
+              branch_id: branchId,
+              branch_name: branchName,
+              address,
+              distance,
+              wait_time,
+            }) => (
+              <li
+                key={branchId}
+                className='w-[40%] flex-shrink-0 items-stretch'
+              >
+                <RecBranchBox
+                  branchId={branchId}
+                  branchName={branchName}
+                  location={address}
+                  distance={distance}
+                  waitingTime={wait_time}
+                />
+              </li>
+            )
+          )
+        )}
       </ul>
     </div>
   );
